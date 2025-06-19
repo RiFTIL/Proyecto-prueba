40739baaeb1751fdceac108eb1f888538ca85b9a
Leonardo Ramirez <leo.ramirez5256@gmail.com>
2025-06-19 15:42:33 -0400

    Arreglos de estilo en el menú

--- Expresos/urls.py
+++ Expresos/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from horariosApp import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('horarios/', views.horarios_views, name='horarios'),
     path('contacto/', views.contacto, name='contacto'),
--- horariosApp/admin.py
+++ horariosApp/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Horario
 # Register your models here.
-admin.site.register(Horario)
+
+# Creamos una clase para personalizar la vista del admin
+class HorarioAdmin(admin.ModelAdmin):
+    # Columnas que se mostrarán en la lista de horarios
+    list_display = ('origen', 'destino', 'dias', 'tarifa_adulto')
+
+    # Filtros que aparecerán en una barra lateral
+    list_filter = ('origen', 'destino')
+
+    # Un campo de búsqueda que buscará por estos campos
+    search_fields = ('origen', 'destino', 'dias')
+
+admin.site.register(Horario, HorarioAdmin)
